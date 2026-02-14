4cac736fa3de12c2987160abea9593afa3c9dcff
Aryan giri <giriaryan763@gmail.com>
2026-02-14 23:48:49 +0530

    production stable build

--- src/app/api/listings/[id]/route.ts
+++ src/app/api/listings/[id]/route.ts
@@ -1,25 +1,30 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
 
 export const dynamic = "force-dynamic";
 
 export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await context.params;
+
   const listing = await prisma.listing.findUnique({
-    where: { id: params.id },
+    where: { id: Number(id) },
   });
 
   return NextResponse.json(listing);
 }
 
 export async function DELETE(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await context.params;
+
   await prisma.listing.delete({
-    where: { id: params.id },
+    where: { id: Number(id) },
   });
 
   return NextResponse.json({ success: true });
